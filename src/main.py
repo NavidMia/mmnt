3db891a188e73a015fad129e76a917ea8ac97383
Navid Mia <navid.mia@outlook.com>
2019-03-02 05:41:41 -0500

    Updates main to use new motor control API and follow mic with slave camera

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@
 # sudo chmod o+rw /dev/bus/usb/001/003
 
 import time
-import math
 import sys
 import usb.core
 import usb.util
@@ -11,90 +10,43 @@ import serial
 from tuning import Tuning
 from motor_control import MotorControl
 
-stepSize = 1.8/8
-stepsPerRev = 360/stepSize
-motor1Id = 0
-motor_cmd = "motor_set"
-THRESHOLD = 20
-CW = -1
-CCW = 1
+TOP_CAM = "0"
+BOT_CAM = "1"
 
 def main():
-    ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1) # Ubuntu VM
+    ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
     time.sleep(3)
     
     mc = MotorControl()
-    mc.runMotors(90, 270)
-    sleep(2)
-    mc.runMotors(MotorControl.CCW, 350, MotorControl.CW, 350)
-    sleep(2)
+    # mc.runMotors(90, 270)
+    # sleep(2)
+    # mc.runMotors(270, 90)
+    # sleep(2)
+
     dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
     if not dev:
-        sys.exit("Could not find ReSpeaker through USB")
-#     ser.write("motor_set 1 1 350")
-#     time.sleep(2)
-#     ser.write("motor_set 0 0 350")
-#     time.sleep(3)
-#     ser.write("motor_set 1 0 350")
-#     time.sleep(2)
-#     ser.write("motor_set 0 1 350")
-    prevAngle = 0;
+        sys.exit("Could not find ReSpeaker Mic Array through USB")
+
     mic = Tuning(dev)
     mic.write("NONSTATNOISEONOFF", 1)
     mic.write("STATNOISEONOFF", 1)
     
+    slaveCam = BOT_CAM;
+    masterCam = TOP_CAM;
+
     while True:
         try:
-            # prev to ref
-            # 0 to 65:
-            # 65 to 0
-            # 350 to 0
-            # 0 to 270
-            rotAngle = 0
-            rotDir = CCW
-            cwDiff = 0
-            ccwDiff = 0
-
-            refAngle = mic.direction
-            print "refAngle:", refAngle, "prevAngle:", prevAngle
-            diff = refAngle - prevAngle
-            if abs(diff) < THRESHOLD:
-                time.sleep(2)
-                continue
-
-            if refAngle < prevAngle:
-                ccwDiff = (360 - prevAngle) + refAngle
-                cwDiff = prevAngle - refAngle
-            else:
-                ccwDiff = refAngle - prevAngle
-                cwDiff = prevAngle + (360 - refAngle)
-
-            if cwDiff < ccwDiff:
-                rotAngle = cwDiff
-                rotDir = CW
-                print "CW rotAngle:", rotAngle
-            else:
-                rotAngle = ccwDiff
-                rotDir = CCW
-                print "CCW rotAngle:", rotAngle
-            if rotAngle > 180:
-                print "~~~~~~~~~~~~ANGLE IS OVER 180 BAD MATH~~~~~~~~~~~~"
-            cmd = buildMotorCommand(motor1Id, rotDir, rotAngle/stepSize)
-            print cmd
-            prevAngle = refAngle
+            targetAngle = mic.direction
             ser.flushInput()
             ser.flushOutput()
-            ser.write(cmd)
-            time.sleep(2) # minimum time for a 360 from a send command
+            if slaveCam == BOT_CAM:
+                mc.botMotor(targetAngle)
+            else:
+                mc.topMotor(targetAngle)
 
         except KeyboardInterrupt:
-            print "terminating"
+            print "Keyboard interrupt! Terminating."
             break
-
-# dir 1 is CCW, else CW
-# mic is CCW
-def buildMotorCommand(motorId, rotDir, steps):
-    return motor_cmd + " " + str(motorId) + " " + str(rotDir) + " " + str(int(steps))
 
 if __name__ == "__main__":
     main()
